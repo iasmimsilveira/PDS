7f19d8655029940c83b09716ec079226f2f6d9a6
Iasmim Silveira <54645836+iasmimsilveira@users.noreply.github.com>
2022-09-22 10:59:31 -0300

    update

--- gradiente.m
+++ gradiente.m
@@ -1,26 +1,25 @@
 clear all
 close all
 clc
-
 pkg load image
 
-img = imread('img_pulso.jpg'); % Fazendo a leitura da imagem
-figure,imshow(img); % Visualizando
+img = imread ('img_dente.jpg');
+figure,imshow(img); c=colorbar();set(c,'FontSize',10);
 
+%Máscara vertical para detectar bordas horizontais
 sH = fspecial('sobel');
 S_H = filter2(sH,img,'same');
 S_H = abs (S_H);
 
-% Máscara horizontal para detectar bordas verticais
-sV = sH;
-S_V = filter2(sH,img,'same');
-S_V = abs (S_H);
+%Máscara horizontal para detectar bordas verticais
+sV = sH';
+S_V = filter2(sV,img,'same');
+S_V = abs(S_V);
 
-% Magnitude gradiente
+%Magnitute do gradiente
 S_H_V = S_H + S_V;
 
-% Sobel
-figure, imshow(S_H, []);
-figure, imshow(S_V, []);
-figure, imshow(S_H_V, []);
-
+%Sobel
+figure,imshow(S_H,[]); c=colorbar();set(c,'FontSize',10);
+figure,imshow(S_V,[]); c=colorbar();set(c,'FontSize',10);
+figure,imshow(S_H_V,[]); c=colorbar();set(c,'FontSize',10);
